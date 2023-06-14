7bef48d4710c639d7976c107e24644aaec86883f
PerttuValtonen <135586722+PerttuValtonen@users.noreply.github.com>
2023-06-14 09:28:54 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Perttu Valtonen on 14.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
